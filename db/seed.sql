ab57b4080a7a0301800ccc9e2c65d3b79f5e1c5d
jdrichards-pursuit <jdrpursuit@gmail.com>
2024-04-15 18:55:57 -0400

    add seeding

--- db/seed.sql
+++ db/seed.sql
@@ -7,8 +7,8 @@ VALUES
 
 INSERT INTO reminders (user_id, title, detail, reminder_time, status)
 VALUES
-(1, 'Quarterly Review', 'Quarterly financial review meeting with the finance department.', '2024-04-015T17:36', 'active'),
-(1, 'Client Call', 'Call with key client to discuss project deliverables and timeline.', '2024-04-015T17:37', 'active'),
-(1, 'Team Lunch', 'Lunch outing with the team for team building.', '2024-04-015T17:38', 'active'),
-(1, 'Code Review', 'Review latest code submissions with the development team.', '2024-04-015T17:39', 'active'),
-(1, 'Product Demo', 'Demonstrate new product features to potential customers.', '2024-04-015T17:40', 'active');
+(1, 'Quarterly Review', 'Quarterly financial review meeting with the finance department.', '2024-04-015T18:54', 'active'),
+(1, 'Client Call', 'Call with key client to discuss project deliverables and timeline.', '2024-04-015T18:55', 'active'),
+(1, 'Team Lunch', 'Lunch outing with the team for team building.', '2024-04-015T18:56', 'active'),
+(1, 'Code Review', 'Review latest code submissions with the development team.', '2024-04-015T18:57', 'active'),
+(1, 'Product Demo', 'Demonstrate new product features to potential customers.', '2024-04-015T18:58', 'active');
